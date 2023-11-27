7e1d9b113904bff639b2c1faf2a831dbd2034535
Landon <landonp95@gmail.com>
2023-11-27 16:48:47 -0500

    remove else, delete message_content intent

--- main.cpp
+++ main.cpp
@@ -8,15 +8,13 @@ int main() {
 
     auto languages = Commands::Language::load_languages();
 
-    dpp::cluster bot(dotenv::env["BOT_TOKEN"], dpp::i_default_intents | dpp::i_message_content);
+    dpp::cluster bot(dotenv::env["BOT_TOKEN"]);
 
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_slashcommand([&bot, &languages](const dpp::slashcommand_t &event) {
         if (const std::string cmdName = event.command.get_command_name(); cmdName == "language") {
             Commands::Language::handle_command(&event, &languages);
-        } else {
-            bot.log(dpp::ll_error, "No idea how but a command was called that does not exist");
         }
     });
 
