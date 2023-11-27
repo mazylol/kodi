cdbe53bf2b0a7677e6dce8987e218a0e054e2c4d
Landon <landonp95@gmail.com>
2023-11-26 19:26:48 -0500

    ran formatting

--- Types.h
+++ Types.h
@@ -1,5 +1,6 @@
 #ifndef TYPES_H
 #define TYPES_H
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -19,7 +20,7 @@ public:
         std::vector<Field> fields;
         std::string thumbnail_url;
 
-        void deserialize(const std::string& input) {
+        void deserialize(const std::string &input) {
             try {
                 nlohmann::json object = nlohmann::json::parse(input);
 
@@ -27,16 +28,16 @@ public:
                 description = object.at("description").get<std::string>();
                 thumbnail_url = object.at("thumbnail_url").get<std::string>();
 
-                const auto&fieldsJson = object.at("fields");
+                const auto &fieldsJson = object.at("fields");
                 fields.clear();
-                for (const auto&fieldJson: fieldsJson) {
+                for (const auto &fieldJson: fieldsJson) {
                     Field field;
                     field.name = fieldJson.at("name").get<std::string>();
                     field.value = fieldJson.at("value").get<std::string>();
                     fields.push_back(field);
                 }
             }
-            catch (const std::exception&e) {
+            catch (const std::exception &e) {
                 std::cerr << "Error parsing JSON: " << e.what() << std::endl;
             }
         }
