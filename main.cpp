baa53cdb4f1e420b706ac5dcbdeeb471a050cb2e
Landon <landonp95@gmail.com>
2023-12-22 16:09:33 -0500

    bot shutdown command, makes my life way easier

--- main.cpp
+++ main.cpp
@@ -14,11 +14,13 @@ int main() {
 
     bot.on_log(dpp::utility::cout_logger());
 
-    bot.on_slashcommand([&languages, &people](const dpp::slashcommand_t &event) {
+    bot.on_slashcommand([&bot, &languages, &people](const dpp::slashcommand_t &event) {
         if (const std::string cmdName = event.command.get_command_name(); cmdName == "language") {
             Commands::Language::handle_command(&event, &languages);
         } else if (cmdName == "person") {
             Commands::Person::handle_command(&event, &people);
+        } else if (cmdName == "shutdown" && dotenv::env["PROD"].empty()) {
+            bot.shutdown();
         }
     });
 
@@ -28,6 +30,12 @@ int main() {
                                        "the language you want");
             Commands::register_command(&bot, &people, "person", "A command for influential programming figures",
                                        "person", "the person you want");
+
+            if (dotenv::env["PROD"].empty()) {
+                bot.guild_command_create(
+                        dpp::slashcommand("shutdown", "Turn off the bot", bot.me.id).set_default_permissions(
+                                dpp::permissions::p_administrator), dotenv::env["GUILD_ID"]);
+            }
         }
 
         bot.set_presence(dpp::presence(dpp::ps_online, dpp::at_custom, "watching you steal from stackoverflow"));
